71566493c0f4d48661a4ca62fdbc59fc584dcb0f
kernsm-bsi <139244341+kernsm-bsi@users.noreply.github.com>
2024-01-09 23:39:21 -0500

    Update msmq_queuejumper.sh

--- modules/msmq_queuejumper.sh
+++ modules/msmq_queuejumper.sh
@@ -5,5 +5,5 @@
 # AUTHOR: Mitchell Kerns
 
 # Check with MSF
-msfconsole -q -x "use auxiliary/scanner/msmq/cve_2023_21554_queuejumper; set RHOSTS $1; run; exit"
+msfconsole -q -x "use auxiliary/scanner/msmq/cve_2023_21554_queuejumper; set RHOSTS $1; run; exit" | sed 's/\x1b\[[0-9;]*m//g'
 
