b1ecfeb1c65f2fae184be098b081233d8d352e9e
kernsm-bsi <139244341+kernsm-bsi@users.noreply.github.com>
2024-01-09 23:40:39 -0500

    Update setup.sh

--- modules/setup.sh
+++ modules/setup.sh
@@ -3,9 +3,17 @@
 sudo apt update -y
 
 # Setup docker and pull the pcredz docker image
-sudo apt install docker.io
+sudo apt install -y docker.io
 sudo docker pull snovvcrash/pcredz
 
 # Edit Responder.conf to 
 sudo sed -i 's/^SMB = On/SMB = OFF/' /etc/responder/Responder.conf
 sudo sed -i 's/^HTTP = On/HTTP = OFF/' /etc/responder/Responder.conf
+
+#Install tool that helps translate SNMP output
+sudo apt-get install snmp-mibs-downloader
+sudo download-mibs
+sudo sed -i '/^mibs :/s/^/#/' /etc/snmp/snmp.conf
+
+# Update Metasploit
+sudo apt install -y metasploit-framework
