4724be683952ea552613a521e45be81150eac579
fancyc-bsi <connor.fancy@bulletproofsi.com>
2024-01-15 11:28:54 -0400

    fixed typo

--- modules/utility-ping-nmap_discovery-alt.sh
+++ modules/utility-ping-nmap_discovery-alt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # STARTFILES
-# targets.txt "Target file description"
+# targets.txt "Target file"
 # ENDFILES
 # AUTHOR: Mitchell Kerns
 
 sudo nmap -n -sn -iL /tmp/targets.txt -oG - | awk '/Up$/{print $2}' > /tmp/up.txt
 echo "Total number of hosts is:"
-ls /tmmp/up.txt
+ls /tmp/up.txt
 wc -l /tmp/up.txt
