4ef678cec4340b8dfe92958d863ab533657abbb3
kernsm-bsi <139244341+kernsm-bsi@users.noreply.github.com>
2024-01-15 09:33:08 -0500

    added alternative discovery scan module

---
+++ modules/utility-ping-nmap_discovery-alt.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# STARTFILES
+# targets.txt "Target file description"
+# ENDFILES
+# AUTHOR: Mitchell Kerns
+
+sudo nmap -n -sn -iL /tmp/targets.txt -oG - | awk '/Up$/{print $2}' > /tmp/up.txt
+echo "Total number of hosts is:"
+wc -l /tmp/up.txt
